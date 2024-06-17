15a453b09ec1af0e0f26cbb062f3935bc5945a1f
ericsonpascua1 <ericsonpascua81@gmail.com>
2024-06-17 15:21:47 +0800

    Update say.js

--- modules/commands/say.js
+++ modules/commands/say.js
@@ -1,39 +1,48 @@
+const axios = require("axios");
+const fs = require("fs");
+const { createReadStream, unlinkSync } = require("fs-extra");
+const path = require("path");
+
 module.exports.config = {
-	name: "say",
-	version: "1.0.0",
-	hasPermssion: 0,
-	credits: "Yan Maglinte",
-	description: "text to voice speech messages",
-  usePrefix: true, //SWITCH TO "false" IF YOU WANT TO DISABLE PREFIX
-	commandCategory: "message",
-	usages: `Text to speech messages`,
-	cooldowns: 5,
-	dependencies: {
-		"path": "",
-		"fs-extra": ""
-	}
+  name: "say",
+  version: "1.0.1",
+  hasPermssion: 0,
+  credits: "ericson pascua",
+  description: "Text to speech",
+  usePrefix: true,
+  commandCategory: "Song/video",
+  usages: "say [Text] or reply",
+  cooldowns: 5,
+  dependencies: {
+    "fs-extra": "",
+  },
 };
 
+module.exports.run = async function ({ api, event, args }) {
+  
+  const msg = args.join(" ");
+  const file = path.join(__dirname, "/cache", `say_${event.senderID}.mp3`);
 
-module.exports.run = async function({
-    api, event, args }) {
+  if(event.type == "message_reply") {
+      const getFile = (await axios.get(`https://translate.google.com/translate_tts?ie=UTF-8&q=${event.messageReply.body}&tl=tl&client=tw-ob`, { responseType: 'arraybuffer'})).data;
+      fs.writeFileSync(file, Buffer.from(getFile, 'utf-8'));
+      api.sendMessage({body: event.messageReply.body, attachment: fs.createReadStream(file)}, event.threadID, (err) => {
+        fs.unlinkSync(file);
+        if (err) return api.sendMessage(`Server error`, event.threadID, event.messageID);
+        }, event.messageID);
+  }
+  else {
     try {
-        const {
-            createReadStream
-            , unlinkSync
-        } = global.nodemodule["fs-extra"];
-        const {
-            resolve
-        } = global.nodemodule["path"];
-        var content = (event.type == "message_reply") ? event.messageReply.body : args.join(" ");
-        var languageToSay = (["ru", "en", "ko", "ja", "tl"].some(item => content.indexOf(item) == 0)) ? content.slice(0, content.indexOf(" ")) : global.config.language;
-        var msg = (languageToSay != global.config.language) ? content.slice(3, content.length) : content;
-        const path = resolve(__dirname, 'cache', `${event.threadID}_${event.senderID}.mp3`);
-        await global.utils.downloadFile(`https://translate.google.com/translate_tts?ie=UTF-8&q=${encodeURIComponent(msg)}&tl=${languageToSay}&client=tw-ob`, path);
-        return api.sendMessage({
-            attachment: createReadStream(path)
-        }, event.threadID, () => unlinkSync(path), event.messageID);
-    } catch (e) {
-        return console.log(e)
-    };
-}
+      if (!msg) {
+        const errorMessage = `Missing text. How to use?\n${global.config.PREFIX}say <text>\n\nExample:\n${global.config.PREFIX}say hello aki`;
+        return api.sendMessage({ body: errorMessage }, event.threadID, event.messageID);
+      } else {
+        const getFile = (await axios.get(`https://translate.google.com/translate_tts?ie=UTF-8&q=${encodeURIComponent(msg)}&tl=tl&client=tw-ob`, { responseType: 'arraybuffer' })).data;
+        fs.writeFileSync(file, Buffer.from(getFile, 'utf-8'));
+        api.sendMessage({ body: msg, attachment: createReadStream(file) }, event.threadID, () => unlinkSync(file), event.messageID);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+};
